e37282f4f232fe0163287f2cd6e05ef0c54fe022
secret-point <secret.point2020@gmail.com>
2024-03-31 18:20:53 -0300

    Initial commit

---
+++ frontend/babel.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    "@vue/cli-plugin-babel/preset",
+    "@babel/preset-env",
+    {
+      targets: {
+        node: "current",
+      },
+    },
+    "@babel/preset-typescript",
+  ],
+};
